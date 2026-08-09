2c42d6b66dd7e62ade72b07c8eef91b4fcdf5745
monk24215 <koubre@gmail.com>
2026-08-09 16:14:14 -0500

    Update template.js

--- composer/src/lib/template.js
+++ composer/src/lib/template.js
@@ -15,15 +15,15 @@ export function renderEmailHtml({ subject, body, preheader }) {
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
-<body style="margin:auto;padding:0;max-width:650px;width:100%;background-color:#ffffff;font-family:Roboto,Arial,sans-serif;line-height:2em;font-size:17px;">
+<body style="margin:auto;padding:0;max-width:600px;width:100%;background-color:#ffffff;color:#333;font-family:PT Sans,Roboto,Arial,sans-serif;line-height:2em;font-size:17.5px;font-weight:400;">
 <div id="preheader" style="color:transparent;display:none;font-size:1px;line-height:1px;max-height:0;max-width:0;opacity:0;overflow:hidden;">${escapeText(pre)}</div>
 
 <table width="100%" border="0" cellpadding="0" cellspacing="0" style="background-color:#ffffff;">
   <tr>
     <td align="center" style="padding:15px 18px;">
-      <table width="650" align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;max-width:650px;font-family:Roboto,Arial,sans-serif;line-height:2em;font-size:17px;background-color:#ffffff;">
+      <table width="650" align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;max-width:650px;margin:auto;padding:0;max-width:600px;width:100%;background-color:#ffffff;color:#333;font-family:PT Sans,Roboto,Arial,sans-serif;line-height:2em;font-size:17.5px;font-weight:400;background-color:#ffffff;">
         <tr>
-          <td align="left" style="font-family:Roboto,Arial,sans-serif;color:#3E3E3E;font-size:17px;line-height:2em;word-wrap:break-word;padding:50px 30px;">${bodyHtml}
+          <td align="left" style="margin:auto;padding:0;max-width:600px;width:100%;background-color:#ffffff;color:#333;font-family:PT Sans,Roboto,Arial,sans-serif;line-height:2em;font-size:17.5px;font-weight:400;word-wrap:break-word;padding:50px 30px;">${bodyHtml}
           <p>&nbsp;</p>  <p>&nbsp;</p>
           </td>
         </tr>
