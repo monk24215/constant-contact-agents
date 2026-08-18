19e939984ee4c5a70c75d96ce903487d09e0fb9b
monk24215 <koubre@gmail.com>
2026-08-18 12:15:52 -0500

    Refactor template.js to clean up HTML structure
    Removed unnecessary <tbody> tags from the template structure.

--- composer/src/lib/template.js
+++ composer/src/lib/template.js
@@ -21,12 +21,10 @@ export function renderEmailHtml({ subject, body, preheader }) {
 
 <div id="preheader" style="color:transparent;display:none;font-size:1px;line-height:1px;max-height:0;max-width:0;opacity:0;overflow:hidden;max-height:0; overflow:hidden; mso-hide:all;">${escapeText(pre)}</div>
 <table width="650" align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;max-width:650px;margin-left:auto;margin-right:auto;background-color:#ffffff;border-color:#ffffff;">
-<tbody>
 <tr>
 <td align="center" style="padding:0px 22px;">
 
 <table width="650" align="center" border="0" cellpadding="0" cellspacing="0" style="width:100%;max-width:650px;background-color:#ffffff;display:block; border:none; border-collapse:collapse;padding:1%;overflow:hidden;border-color:#ffffff;">
-<tbody>
 <tr>
 <td align="left" style="color:#333333;font-family:'PT Sans',Roboto,Arial,sans-serif;font-size:17px;font-weight:400;line-height:1.7em;letter-spacing:0;text-align:left;word-wrap:break-word;padding:0px 20px;">${bodyHtml}
           </td>
